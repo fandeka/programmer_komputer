474b76445e0de0f7e3377ef529fa5f28c30584e5
atho1414 <urrohmanqtho@gmail.com>
2018-10-11 11:54:51 +0700

    test

--- latihan_git.php
+++ latihan_git.php
@@ -1,14 +1,13 @@
 <?php
-<<<<<<< HEAD
+
 
 echo "ini latihan git";
 
 echo "ini source code dari aldilla"
 echo "ini source code dari athour";
-<<<<<<< HEAD
+
 echo "ini source code dari ridwan";
 echo "masuk pa eko";
-=======
 echo "ini dari amin sochip";
 
 echo "ini source code dari sugik";
@@ -29,6 +28,5 @@ echo "ini saya";
 
 
 echo "MASUK... PAK... EKO.....";
-
->>>>>>> a249daa3856d064c727b23da44dc73f9034cd1d5
+echo "jam pulang...pulang..pulang";
 ?>
