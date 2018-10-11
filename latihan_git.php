4ee21aef6e4d0b08b9e77473cef9ef2f1750cc7f
Mr Cracker <ledwahyudi@gmail.com>
2018-10-11 11:42:08 +0700

    EKO

--- latihan_git.php
+++ latihan_git.php
@@ -1,19 +1,3 @@
 <?php
-
-echo "ini latihan git";
-
-echo "ini source code dari aldilla"
-echo "ini source code dari athour";
-echo "ini dari amin sochip";
-
-echo "ini source code dari sugik";
-
-echo "ini source code dari yudi";
-echo "ini source code dari raden samudra";
-<<<<<<< HEAD
 echo "PULANG... PAK... EKO.....";
-=======
-
-echo "ini source code dari ahmad";
->>>>>>> master
 ?>
