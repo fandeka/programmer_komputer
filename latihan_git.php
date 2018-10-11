b8ee91fc0a6cb14a6f6ae499dd6d29edc818e424
joko sihono <jokosihonowae@gmail.com>
2018-10-11 11:17:47 +0700

    dari joko

--- latihan_git.php
+++ latihan_git.php
@@ -7,5 +7,5 @@ echo "ini source code dari ridwan";
 echo "ini source code dari pak ilyas";
 echo "ini source code dari aldilla";
 echo "inoi source code dari radenmas Endro";
-
+echo "ini source code dari joko";
 ?>
