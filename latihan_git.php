8229979487fc0cf7c0d5241fc71ecc953624261a
prajurit <aa@gmail.com>
2018-10-11 11:15:29 +0700

    Radenmas Endro

--- latihan_git.php
+++ latihan_git.php
@@ -6,4 +6,6 @@ echo "ini source code dari athour";
 echo "ini source code dari ridwan";
 echo "ini source code dari pak ilyas";
 echo "ini source code dari aldilla";
+echo "inoi source code dari radenmas Endro";
+
 ?>
