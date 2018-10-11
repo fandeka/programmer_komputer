cb5842613749c93add9ed46b5319d9a02b6436a2
prajurit <aa@gmail.com>
2018-10-11 11:44:43 +0700

    nambah

--- latihan_git.php
+++ latihan_git.php
@@ -10,14 +10,11 @@ echo "ini source code dari sugik";
 
 echo "ini source code dari yudi";
 echo "ini source code dari raden samudra";
-<<<<<<< HEAD
+
 echo "PULANG... PAK... EKO.....";
-=======
 
 echo "ini source code dari ahmad";
-<<<<<<< HEAD
+
 echo "ini source code dari Radenbeii Endro";
-=======
->>>>>>> master
->>>>>>> 16d5d8cea808ac0976375e4e2380d97b3323d9a3
+echo "adak..adak...adak.........ayowwwhhh.ayowh.."
 ?>
